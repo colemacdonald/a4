7482a71f63cf61c1757ef5537fcdc2080c65e6dd
Cole Macdonald <colesinbox@gmail.com>
2017-07-30 16:13:39 -0700

    working on stor

--- storuvfs.c
+++ storuvfs.c
@@ -23,11 +23,17 @@ void                pack_current_datetime(unsigned char *entry);
 int                 next_free_block(int *FAT, int max_blocks);
 void                sseek(FILE * f, int len, int origin);
 void                sread(void * buffer, size_t sizeofelements, size_t num, FILE *f);
+void                swrite(void * buffer, size_t sizeofelements, size_t num, FILE *f);
 void                convertToNet(superblock_entry_t * sb);
-unsigned int        findFileOnDisk(diskimage_t * image, char * filename);
-void                catFile(diskimage_t * image, unsigned int start_block);
-unsigned long int   read_fat_entry(diskimage_t * image, unsigned long int entry);
 void                convertToNetDE(directory_entry_t * de);
+void                convertToHostDE(directory_entry_t * de);
+unsigned int        findFileOnDisk(diskimage_t * image, char * filename);
+unsigned long int   read_fat_entry(diskimage_t * image, unsigned long int entry);
+directory_entry_t   create_directory_entry(diskimage_t * image, char * filename);
+void                write_file_to_image(diskimage_t * image, char * filename, FILE * src_file);
+void                write_to_fat(diskimage_t * image, unsigned long int cur_entry, unsigned long int next_entry);
+void                read_fat(diskimage_t * image, int * FAT);
+void                write_fat(diskimage_t * t, int * FAT);
 
 /************************* FUNCTION IMPLEMENTATIONS *************************/
 
@@ -70,8 +76,224 @@ int next_free_block(int *FAT, int max_blocks) {
             return i;
         }
     }
-
     return -1;
+}
+
+/*
+ * Safe seek
+ * Runs fseek with input params and handles errors
+ */
+void sseek(FILE * f, int len, int origin)
+{
+    if(0 != fseek(f, len, origin))
+    {
+        fprintf(stderr, "Seek failed.\n");
+        exit(1);
+    }
+}
+
+/*
+ * Safe read
+ * Runs fread with input params and handles error
+ */
+void sread(void * buffer, size_t sizeofelements, size_t num, FILE *f)
+{
+    if( fread(buffer, sizeofelements, num, f) != num )
+    {
+        fprintf(stderr, "Read failed.\n");
+        exit(1);
+    }
+}
+
+void swrite(void * buffer, size_t sizeofelements, size_t num, FILE *f)
+{
+    if ( fwrite(buffer, sizeofelements, num, f) != num )
+    {
+        fprintf(stderr, "Write failed.\n");
+        exit(1);
+    }
+}
+
+/*
+ * Converts superblock to network byte order
+ */
+void convertToNet(superblock_entry_t * sb)
+{
+    sb->block_size = htons(sb->block_size);
+    sb->num_blocks = htonl(sb->num_blocks);
+    sb->fat_start = htonl(sb->fat_start);
+    sb->fat_blocks = htonl(sb->fat_blocks);
+    sb->dir_start = htonl(sb->dir_start);
+    sb->dir_blocks = htonl(sb->dir_blocks);
+}
+
+/*
+ * Convert directory entry to network byte order
+ */
+void convertToNetDE(directory_entry_t * de)
+{
+    de->start_block = htonl(de->start_block);
+    de->num_blocks = htonl(de->num_blocks);
+    de->file_size = htonl(de->file_size);
+}
+
+/*
+ * Locates filename on image.
+ * Returns location of first block or 0xffffffff if end of file or 0 if not found.
+ */
+unsigned int findFileOnDisk(diskimage_t * image, char * filename)
+{
+    int i, j;
+    // for each block of root
+    for(i = 0; i < image->sb->dir_blocks; i++)
+    {
+        // for each directory entry of block
+        for(j = 0; j < image->sb->block_size/SIZE_DIR_ENTRY; j++)
+        {
+            sseek(image->f, ((image->sb->dir_start + i) * image->sb->block_size + j * SIZE_DIR_ENTRY), SEEK_SET);
+            directory_entry_t de;
+            sread(&de, sizeof(directory_entry_t), 1, image->f);
+
+            if(de.status == DIR_ENTRY_AVAILABLE || strcmp(de.filename, filename) != 0)
+                continue;
+
+            convertToNetDE(&de);
+            return de.start_block;
+        }
+    }
+    return 0;
+}
+
+/*
+ * Creates a directory entry for the file and returns the first block
+ */
+// directory_entry_t create_directory_entry(diskimage_t * image, char * filename)
+// {
+//     directory_entry_t de;
+//     de.status = DIR_ENTRY_NORMALFILE;
+//     de.start_block = next_free_block;
+// }
+
+void write_file_to_image(diskimage_t * image, char * filename, FILE * src_file)
+{
+    unsigned long int de_write_byte = 0;
+    int i, j;
+    // for each block of root
+    for(i = 0; i < image->sb->dir_blocks; i++)
+    {
+        // for each directory entry of block
+        for(j = 0; j < image->sb->block_size/SIZE_DIR_ENTRY; j++)
+        {
+            sseek(image->f, ((image->sb->dir_start + i) * image->sb->block_size + j * SIZE_DIR_ENTRY), SEEK_SET);
+            directory_entry_t de;
+            sread(&de, sizeof(directory_entry_t), 1, image->f);
+
+            if(de.status == DIR_ENTRY_AVAILABLE)
+            {
+                de_write_byte = (image->sb->dir_start + i) * image->sb->block_size + j * SIZE_DIR_ENTRY;
+                break;
+            }
+        }
+        if(de_write_byte != 0)
+            break;
+    }
+
+    int FAT[image->sb->fat_blocks * image->sb->block_size / SIZE_FAT_ENTRY];
+    read_fat(image, FAT);
+
+    printf("DE start: %lu\n", de_write_byte);
+
+    if(de_write_byte == 0)
+    {
+        fprintf(stderr, "No room for directory entry.\n");
+        exit(1);
+    }
+
+    char write_buffer[image->sb->block_size + 1];
+    directory_entry_t de;
+
+    de.status = DIR_ENTRY_NORMALFILE;
+
+    int k = image->sb->fat_start;
+    sseek(image->f, image->sb->fat_start * image->sb->block_size, SEEK_SET);
+    de.start_block = next_free_block(FAT, image->sb->num_blocks);
+    printf("%d\n", de.start_block);
+    de.num_blocks = 1;
+    strcpy(de.filename, filename);
+
+    sseek(src_file, 0L, SEEK_SET);
+    int bytes_read;
+
+    //read first block
+    bytes_read = fread(&write_buffer, 1, image->sb->block_size, src_file);
+    write_buffer[bytes_read] = '\0';
+
+    //write first block
+    sseek(image->f, de.start_block * image->sb->block_size, SEEK_SET);
+    swrite(&write_buffer, sizeof(char), bytes_read, image->f);
+
+    unsigned int write_block = de.start_block;
+
+    while( (bytes_read = fread(&write_buffer, 1, image->sb->block_size, src_file)) > 0)
+    {
+        int tmp = image->sb->fat_start * image->sb->block_size;
+        unsigned int next_block = next_free_block(FAT, image->sb->num_blocks);
+        
+        write_buffer[bytes_read] = '\0';
+        if(next_block == 0)
+        {
+            fprintf(stderr, "Not enough room for file.\n");
+            exit(1);
+        }
+
+        //write_to_fat(image, write_block, next_block);
+
+        FAT[write_block] = ntohl(next_block);
+        printf("Saving %x to %x\n", next_block, write_block);
+        
+        sseek(image->f, write_block * image->sb->block_size, SEEK_SET);
+        swrite(&write_buffer, sizeof(char), bytes_read, image->f);
+
+        write_block = next_block;
+        de.num_blocks++;
+    }
+    FAT[write_block] = FAT_LASTBLOCK;
+    write_fat(image, FAT);
+    //write_to_fat(image, write_block, FAT_LASTBLOCK);
+    de.file_size = (de.num_blocks - 1) * image->sb->block_size + bytes_read;
+    pack_current_datetime(de.create_time);
+    pack_current_datetime(de.modify_time);
+
+    sseek(image->f, de_write_byte, SEEK_SET);
+    convertToHostDE(&de);
+    swrite(&de, sizeof(de), 1, image->f);
+}
+
+void write_to_fat(diskimage_t * image, unsigned long int cur_entry, unsigned long int next_entry)
+{
+    sseek(image->f, image->sb->fat_start * image->sb->block_size + cur_entry * SIZE_FAT_ENTRY, SEEK_SET);
+    next_entry = ntohl(next_entry);
+    printf("Writing %lx to %lx\n", next_entry, cur_entry);
+    swrite(&next_entry, sizeof(unsigned long int), 1, image->f);
+}
+
+void convertToHostDE(directory_entry_t * de)
+{
+    de->start_block = ntohl(de->start_block);
+    de->num_blocks = ntohl(de->num_blocks);
+    de->file_size = ntohl(de->file_size);
+}
+
+void read_fat(diskimage_t * image, int * FAT)
+{
+    sseek(image->f, image->sb->fat_start * image->sb->block_size, SEEK_SET);
+    sread(FAT, sizeof(int), image->sb->fat_blocks * image->sb->block_size / SIZE_FAT_ENTRY, image->f);
+}
+
+void write_fat(diskimage_t * image, int * FAT)
+{
+    sseek(image->f, image->sb->fat_start * image->sb->block_size, SEEK_SET);
+    swrite(FAT, sizeof(int), image->sb->fat_blocks * image->sb->block_size / SIZE_FAT_ENTRY, image->f);
 }
 
 /*************************** MAIN ***************************/
@@ -111,7 +333,7 @@ int main(int argc, char *argv[]) {
 
 /********************* END Z **********************/
 
-    if( (image.f = fopen(imagename, "wb+")) == NULL )
+    if( (image.f = fopen(imagename, "rb+")) == NULL )
     {
         fprintf(stderr, "Specified image could not be opened.\n");
         exit(1);
@@ -121,11 +343,12 @@ int main(int argc, char *argv[]) {
     {
         fprintf(stderr, "Specified source file could not be found.\n");
     }
-
+ 
     image.imagename = imagename;
     sseek(image.f, 0L, SEEK_SET);
     // read in super block
     sread(image.sb, sizeof(superblock_entry_t), 1, image.f);
+
     // validate image
     if(strncmp(image.sb->magic, FILE_SYSTEM_ID, 8) != 0)
     {
@@ -135,6 +358,14 @@ int main(int argc, char *argv[]) {
 
     convertToNet(image.sb);
 
+    // check file doesn't already exist
+    if(findFileOnDisk(&image, filename) != 0)
+    {
+        fprintf(stderr, "File already on specified image.\n");
+        exit(1);
+    }
+
+    write_file_to_image(&image, filename, src_file);
 
     return 0; 
 }
